02d40c7a13d904f307ea9ffc432dda94fe06c5fa
Akshay Suresh <akshay721@gmail.com>
2021-12-28 04:35:07 -0500

    Added functionality to inject periodic signal into real data set.

--- executables/inject_signal.py
+++ executables/inject_signal.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 """
-Simulate an artificial data set and save it to disk as a filterbank file.
+Inject a periodic signal of known properties into a real-world filterbank data file.
 
 Run using the following syntax.
-python simulate_data.py -i <Configuration script of inputs> | tee <Log file>
+python inject_signal.py -i <Configuration script of inputs> | tee <Log file>
 """
 from __future__ import print_function
 from __future__ import absolute_import
@@ -12,14 +12,13 @@ from modules.read_config import read_config
 from modules.general_utils import setup_logger_stdout, create_dir
 # Standard imports
 from blimpy import Waterfall
-from blimpy.io.sigproc import generate_sigproc_header
 from argparse import ArgumentParser
 import os, logging, time, sys
 import numpy as np
 ##############################################################
 def myexecute(inputs_cfg):
     """
-    Primary function that handles synthetic data generation.
+    Primary function that handles fake signal injection.
 
     Parameters
     ----------
@@ -34,41 +33,47 @@ def myexecute(inputs_cfg):
     hotpotato = set_defaults(hotpotato)
     logger = setup_logger_stdout() # Set logger output to stdout().
 
-    # No. of channels in which to inject a periodic signal
-    N_inject = len(hotpotato['inject_channels'])
+    # Read data from a filterbank file. Set max data load size to 15 GB.
+    wat  = Waterfall(hotpotato['DATA_DIR'] + '/' + hotpotato['datafile'], max_load=15.0)
+    header = wat.header
+    Nchans = header['nchans'] # No. of spectral channels
+    Nsamples  = wat.n_ints_in_file # No. of time samples
+    tsamp = header['tsamp'] # Sampling time (s)
+    times = np.arange(Nsamples)*tsamp # 1D array of times (s)
+    data = wat.data[:,0,:].T # Data shape = (Nchans, Nsamples)
 
-    times = np.arange(hotpotato['N_samples']) * hotpotato['t_samp'] # 1D array of times (s)
-    # Generate Gaussian white noise background with zero mean and unit variance.
-    data = np.random.randn(hotpotato['N_chans'], hotpotato['N_samples'])
-    logger.info('Background Gaussian white noise data generated.')
+    # Median bandpass
+    logger.info('Computing median bandpass')
+    median_bp = np.median(data, axis=1)
+    # Bandpass correction
+    data = (data - median_bp[:,None])/median_bp[:,None]
+    logger.info('Bandpass correction done.')
+    # Normalize the data in each channel to zero median and unit variance.
+    data = (data - np.median(data, axis=1)[:,None])/np.std(data, axis=1)[:,None]
+    logger.info('Channel-wise data normalized to zero mean and unit variance.')
+
     # Inject boxcar periodic signals of constant amplitude.
+    N_inject = len(hotpotato['inject_channels'])
     for i in range(N_inject):
         chan = hotpotato['inject_channels'][i] # Channel index into which a periodic signal must be injected
         phi = times/hotpotato['periods'][i] % 1.0 # Convert times to phase values.
         indices = np.where( np.logical_and(phi>=hotpotato['initial_phase'][i]-0.5*hotpotato['duty_cycles'][i], phi<hotpotato['initial_phase'][i]+0.5*hotpotato['duty_cycles'][i]) )
-        data[chan][indices] += hotpotato['pulse_SNR'][i]
+        # Injected signal =
+        data[chan, indices] += hotpotato['pulse_SNR'][i]
         logger.info('Injected P = %.2f s signal into channel %d.'% (hotpotato['periods'][i], chan))
-    data = data.T.reshape((data.T.shape[0], 1, data.T.shape[1])) # New data shape = (Nsamples, Npol, Nchans)
-
-    # Build file header.
-    header = {'machine_id': 0, 'telescope_id': 0, 'data_type': 1, 'nbits': 32, 'nifs': 1}
-    header['fch1'] = hotpotato['fch1']
-    header['foff'] = hotpotato['foff']
-    header['tsamp'] = hotpotato['t_samp']
-    header['nchans'] = hotpotato['N_chans']
-    header['nsamples'] = hotpotato['N_samples']
-    header['source_name'] = hotpotato['source_name']
-    header['tstart'] = hotpotato['tstart']
+    wat.data = data.T.reshape((Nsamples, wat.header['nifs'], Nchans))
 
     create_dir(hotpotato['OUTPUT_DIR'])
-    # Construct a Waterfall object that will be written to disk as a filterbank file.
-    wat = Waterfall() # Empty Waterfall object
-    wat.header = header
-    filename_out = hotpotato['OUTPUT_DIR']+ '/' + hotpotato['basename'] + '.fil'
-    with open(filename_out, 'wb') as fh:
-        logger.info('Writing filterbank file to %s'% (hotpotato['OUTPUT_DIR']))
-        fh.write(generate_sigproc_header(wat)) # Trick Blimpy into writing a sigproc header.
-        np.float32(data.ravel()).tofile(fh)
+    out_filename = hotpotato['OUTPUT_DIR'] + '/' + hotpotato['basename'] + hotpotato['output_ext']
+    # Write data to disk.
+    if hotpotato['output_ext'] == '.fil':
+        wat.write_to_fil(out_filename)
+        logger.info('%s written to disk'% (out_filename))
+    elif hotpotato['output_ext'] == '.h5':
+        wat.write_to_hdf5(out_filename)
+        logger.info('%s written to disk'% (out_filename))
+    else:
+        logger.error('Output file format not recognized. Allowed file formats: .fil, .h5')
 
     # Calculate total run time for the code.
     prog_end_time = time.time()
@@ -84,6 +89,15 @@ def set_defaults(hotpotato):
     hotpotato : dictionary
          Dictionary of input parameters gathered from a configuration script
     """
+    if hotpotato['output_ext']=='':
+        if '.fil' in hotpotato['datafile']:
+            hotpotato['output_ext'] = '.fil'
+        elif '.h5' in hotpotato['datafile']:
+            hotpotato['output_ext'] = '.h5'
+        else:
+            hotpotato['output_ext'] = '.fil'
+    if hotpotato['OUTPUT_DIR']=='':
+        hotpotato['OUTPUT_DIR'] = hotpotato['DATA_DIR']
     if hotpotato['inject_channels']=='':
         hotpotato['inject_channels'] = []
     if hotpotato['periods']=='':
@@ -94,16 +108,11 @@ def set_defaults(hotpotato):
         hotpotato['pulse_SNR'] = []
     if hotpotato['initial_phase']=='':
         hotpotato['initial_phase'] = []
-    if hotpotato['source_name']=='':
-        hotpotato['source_name'] = 'Unknown'
-    if hotpotato['tstart']=='':
-        hotpotato['tstart'] = 0.0
     return hotpotato
-
 ##############################################################
 def main():
-    """ Command line tool for running simulate_data.py """
-    parser = ArgumentParser(description="Generate an artificial data set.")
+    """ Command line tool for running inject_signal.py """
+    parser = ArgumentParser(description="Inject a fake periodic signal into real-world data.")
     optional = parser._action_groups.pop()
     required = parser.add_argument_group('required arguments')
     required.add_argument('-i', action='store', required=True, dest='inputs_cfg', type=str,
