e8adae5283f45db407b1c41c6a6767ed6563fe9e
Akshay Suresh <akshay721@gmail.com>
2022-02-19 07:31:45 -0500

    Plotting update

--- executables/modules/plotting.py
+++ executables/modules/plotting.py
@@ -54,13 +54,13 @@ def scatterplot_period_radiofreq(cand_periods, cand_freqs, cand_snrs, cand_flags
     fig1 = plt.figure(1)
     # Plot fundamental frequencies.
     f_idx = np.where(cand_flags=='F')[0]
-    plt.scatter(x=cand_periods[f_idx], y=cand_freqs[f_idx], c=cand_snrs[f_idx], marker='o', cmap=cmap, norm=norm)
+    plt.scatter(x=cand_periods[f_idx], y=cand_freqs[f_idx], c=cand_snrs[f_idx], s=38, marker='o', cmap=cmap, norm=norm)
     # Plot sub-harmonics.
     subharm_idx = np.where(cand_flags=='S')[0]
-    plt.scatter(x=cand_periods[subharm_idx], y=cand_freqs[subharm_idx], c=cand_snrs[subharm_idx], marker='+', cmap=cmap, norm=norm)
+    plt.scatter(x=cand_periods[subharm_idx], y=cand_freqs[subharm_idx], c=cand_snrs[subharm_idx], s=38, marker='+', cmap=cmap, norm=norm)
     # Plot harmonics.
     harm_idx = np.where(cand_flags=='H')[0]
-    plt.scatter(x=cand_periods[harm_idx], y=cand_freqs[harm_idx], c=cand_snrs[harm_idx], marker='x', cmap=cmap, norm=norm)
+    plt.scatter(x=cand_periods[harm_idx], y=cand_freqs[harm_idx], c=cand_snrs[harm_idx], s=38, marker='x', cmap=cmap, norm=norm)
     # Set up colorbar.
     cbar = plt.colorbar(plt.cm.ScalarMappable(cmap = cmap, norm = norm))
     cbar.set_label('Matched filtering S/N', fontsize=16)
