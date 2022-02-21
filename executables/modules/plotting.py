7647adf5ae2c390b3d9c64dad6b56e6d7c8c6aa1
Akshay Suresh <akshay721@gmail.com>
2022-02-20 23:47:03 -0500

    Added legend

--- executables/modules/plotting.py
+++ executables/modules/plotting.py
@@ -51,16 +51,23 @@ def scatterplot_period_radiofreq(cand_periods, cand_freqs, cand_snrs, cand_flags
     cmap = plt.cm.cividis
     norm = mpl.colors.Normalize(vmin = np.min(cand_snrs), vmax = np.max(cand_snrs))
     # Begin plotting.
-    fig1 = plt.figure(1)
+    fig1 = plt.figure(1, figsize=(7,6))
+    marker_count = 0 # Keep track of the number of distinct marker shapes to plot.
     # Plot fundamental frequencies.
     f_idx = np.where(cand_flags=='F')[0]
-    plt.scatter(x=cand_periods[f_idx], y=cand_freqs[f_idx], c=cand_snrs[f_idx], s=38, marker='o', cmap=cmap, norm=norm)
+    if len(f_idx) > 0:
+        plt.scatter(x=cand_periods[f_idx], y=cand_freqs[f_idx], c=cand_snrs[f_idx], s=38, marker='o', cmap=cmap, norm=norm)
+        marker_count += 1
     # Plot sub-harmonics.
     subharm_idx = np.where(cand_flags=='S')[0]
-    plt.scatter(x=cand_periods[subharm_idx], y=cand_freqs[subharm_idx], c=cand_snrs[subharm_idx], s=38, marker='+', cmap=cmap, norm=norm)
+    if len(subharm_idx) > 0:
+        plt.scatter(x=cand_periods[subharm_idx], y=cand_freqs[subharm_idx], c=cand_snrs[subharm_idx], s=38, marker='+', cmap=cmap, norm=norm)
+        marker_count += 1
     # Plot harmonics.
     harm_idx = np.where(cand_flags=='H')[0]
-    plt.scatter(x=cand_periods[harm_idx], y=cand_freqs[harm_idx], c=cand_snrs[harm_idx], s=38, marker='x', cmap=cmap, norm=norm)
+    if len(harm_idx) > 0:
+        plt.scatter(x=cand_periods[harm_idx], y=cand_freqs[harm_idx], c=cand_snrs[harm_idx], s=38, marker='x', cmap=cmap, norm=norm)
+        marker_count += 1
     # Set up colorbar.
     cbar = plt.colorbar(plt.cm.ScalarMappable(cmap = cmap, norm = norm))
     cbar.set_label('Matched filtering S/N', fontsize=16)
@@ -73,6 +80,12 @@ def scatterplot_period_radiofreq(cand_periods, cand_freqs, cand_snrs, cand_flags
     # Set axes limits.
     plt.gca().set_xlim( (min_period, max_period) )
     plt.gca().set_ylim( (min_freq, max_freq) )
+    # Show legend.
+    if marker_count>1:
+        leg = plt.legend(bbox_to_anchor=(0.0, -0.22, 1.1, 0.1), loc='center', ncol=3, fancybox=True, frameon=True,
+                         borderpad=0.3, fontsize=14, handletextpad=0.2, columnspacing=2.0)
+        leg.get_frame().set_edgecolor('silver')
+    # Set tight layout for figure.    
     plt.tight_layout()
     # Save plot to disk.
     for format in plot_formats:
